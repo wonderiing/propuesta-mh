22018335eaeed387a1b6605d858394c727a63f9f
wonderiing <ca223871@gmail.com>
2025-02-23 18:09:12 -0600

    cors fix

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI, UploadFile, File, Body
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.services import process_cv
 from app.models import extract_text_from_pdf, extract_text_from_docx
 from app.schemas import FeedbackRequest
 
 app = FastAPI()
+
+# Configuración de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
 
 @app.post("/procesar-cv/")
 async def procesar_cv(
