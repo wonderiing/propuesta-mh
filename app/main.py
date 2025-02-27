bd2a860b545d5452d584de0c061752695701d84d
wonderiing <ca223871@gmail.com>
2025-02-27 01:24:09 -0600

    llama 3.2:1b swap

--- app/main.py
+++ app/main.py
@@ -9,12 +9,12 @@ from app.schemas import FeedbackRequest
 
 
 app = FastAPI()
-model = "tinyllama"
-params = "3b"
+model = "llama3.2"
+params = "1b"
 
 try:
-    ollama.pull(f"{model}")
-    print(f"Modelo {model} descargado exitosamente.")
+    ollama.pull(f"{model}:{params}")
+    print(f"Modelo {model}:{params} descargado exitosamente.")
 except Exception as e:
     print(f"Error al descargar el modelo: {e}")
 
