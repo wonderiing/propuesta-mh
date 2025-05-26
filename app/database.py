2f648f8ba9c7369073a4d76dc0283654b2d956f7
wndr <76233612+wonderiing@users.noreply.github.com>
2025-05-25 19:42:34 -0600

    Update database.py

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:ysQxMqibJSVlwGVrvzFbfNpcXQlkOFQhu@caboose.proxy.rlwy.net:29838/railway")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
